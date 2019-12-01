d9988c6cc4fed77aff2af710e399f46c0446275f
medhasm <57920502+medhasm@users.noreply.github.com>
2019-12-01 23:03:59 +0200

    Add files via upload

---
+++ myBank.h
@@ -0,0 +1,16 @@
+#ifndef MYBANK_H
+#define MYBANK_H
+#define Counter 50
+#define ROW_size 2
+#define COL_size 50
+int Account_Number;
+double BA[COL_size][ROW_size];
+void O();
+void B();
+void D();
+void W();
+void C();
+void I();
+void P();
+void E();
+#endif
